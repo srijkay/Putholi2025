eab4cf34e0dc5e875b8d62f131c0580f4d06f6b1
vijayalakshmi <pusapativijayalakshmi0508@gmail.com>
2026-07-07 14:17:44 +0530

    changes on fund allotment dashboard

--- putholi-ui/src/env.js
+++ putholi-ui/src/env.js
@@ -2,14 +2,21 @@
   window.__env = window.__env || {};
 
   // API url (LOCAL ENVIRONMENT)
-  window.__env.apiUrl = 'http://localhost:9000/api';
+  // window.__env.apiUrl = 'http://localhost:9000/api';
+  // window.__env.adminUrl = "http://localhost:4200";
 
-  window.__env.adminUrl = "http://localhost:9100/admin";
+  // API url (PROD ENVIRONMENT)
+  window.__env.apiUrl = 'https://putholitrust.com/api';
+  window.__env.adminUrl = "https://putholitrust.com/admin";
 
-  window.__env.build_Version = '1.0.0',
-    window.__env.build_Date = '2023-08-27T00:00:00.000+0000'
+  window.__env.build_Version = '2.0.0',
+  window.__env.build_Date = '2026-06-12T00:00:00.000+0000'
 
-  window.__env.ccavenue_redirect_url = "https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"
+  //payment gateway redirect url for testing
+  // window.__env.ccavenue_redirect_url = "https://test.ccavenue.com/transaction/transaction.do?command=initiateTransaction"
+
+  //payment gateway redirect url for production
+  window.__env.ccavenue_redirect_url = "https://secure.ccavenue.com/transaction/transaction.do?command=initiateTransaction"
 
   // Whether or not to enable debug mode
   // Setting this to false will disable console output
